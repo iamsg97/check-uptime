f2bc83eb670c0629a3b8c82d470df4dce49e90db
iamsg97 <iamsg97@outlook.com>
2025-06-29 18:07:29 +0000

    feat: initialize Svelte project with basic structure and configuration

---
+++ apps/ping-client/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    let count = $state(0);
+</script>
+
+<h1>This is weirdly simple and complex</h1>
+<button onclick={() => count++}>
+    clicks: {count}
+</button>
---
+++ apps/ping-client/src/routes/dashboard/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import {onMount} from 'svelte';
+    import {page} from '$app/stores';
+
+    let currentPage = $state<string>();
+
+    onMount(() => {
+        currentPage = $page.url.pathname;
+    });
+</script>
+
+<div>
+    <h2>This is from dashboard!</h2>
+    <p>Experimenting</p>
+    <strong>This is the current page route:</strong> {currentPage}
+</div>
