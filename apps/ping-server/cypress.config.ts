16c9f756fcc8f1e6c2c57b076a8ae0d3c1d6a930
iamsg97 <iamsg97@outlook.com>
2025-08-03 03:47:28 +0000

    feat: cypress types integration

--- apps/ping-server/cypress.config.ts
+++ apps/ping-server/cypress.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+            config.env = {
+                BASE_URL: 'http://localhost:3000',
+                API_PREFIX: '/api/v1',
+            }
+            return config
+        },
     },
-  },
-});
+})
